7f2ea9e6be4b1070cc613b07fa898b90ef146129
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-02-27 09:11:11 +0100

    Add client_id.

--- 03_lesson/ConferenceCentral/src/main/webapp/js/app.js
+++ 03_lesson/ConferenceCentral/src/main/webapp/js/app.js
@@ -86,7 +86,7 @@ app.constant('HTTP_ERRORS', {
  */
 app.factory('oauth2Provider', function ($modal) {
     var oauth2Provider = {
-        CLIENT_ID: 'replace with your client id',
+        CLIENT_ID: '579085138666-94p04u17ru5fgarlj7ddba9ldjpke974.apps.googleusercontent.com',
         SCOPES: 'https://www.googleapis.com/auth/userinfo.email profile',
         signedIn: false
     };
