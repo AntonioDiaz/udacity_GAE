8a7863ecf132b45dee71345b53d113853365b27d
AntonioDiaz <antonio.diaz.arroyo@gmail.com>
2016-02-27 10:23:41 +0100

    Update profile.

--- 03_lesson/ConferenceCentral/src/main/java/com/google/devrel/training/conference/domain/Profile.java
+++ 03_lesson/ConferenceCentral/src/main/java/com/google/devrel/training/conference/domain/Profile.java
@@ -1,35 +1,40 @@
 package com.google.devrel.training.conference.domain;
 
+import com.google.devrel.training.conference.Constants;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 
-
 @Entity
 public class Profile {
-	
+
 	String displayName;
 	String mainEmail;
 	TeeShirtSize teeShirtSize;
 
 	@Id
 	String userId;
-    
-    /**
-     * Public constructor for Profile.
-     * @param userId The user id, obtained from the email
-     * @param displayName Any string user wants us to display him/her on this system.
-     * @param mainEmail User's main e-mail address.
-     * @param teeShirtSize The User's tee shirt size
-     * 
-     */
-    public Profile (String userId, String displayName, String mainEmail, TeeShirtSize teeShirtSize) {
-    	this.userId = userId;
-    	this.displayName = displayName;
-    	this.mainEmail = mainEmail;
-    	this.teeShirtSize = teeShirtSize;
-    }
-    
+
+	/**
+	 * Public constructor for Profile.
+	 * 
+	 * @param userId
+	 *            The user id, obtained from the email
+	 * @param displayName
+	 *            Any string user wants us to display him/her on this system.
+	 * @param mainEmail
+	 *            User's main e-mail address.
+	 * @param teeShirtSize
+	 *            The User's tee shirt size
+	 * 
+	 */
+	public Profile(String userId, String displayName, String mainEmail, TeeShirtSize teeShirtSize) {
+		this.userId = userId;
+		this.displayName = displayName;
+		this.mainEmail = mainEmail;
+		this.teeShirtSize = teeShirtSize;
+	}
+
 	public String getDisplayName() {
 		return displayName;
 	}
@@ -46,9 +51,18 @@ public class Profile {
 		return userId;
 	}
 
-	/**
-     * Just making the default constructor private.
-     */
-    private Profile() {}
+	/** just making the default constructor private. */
+	private Profile() {
+	}
+
+	public void update(String displayName2, TeeShirtSize teeShirtSize2) {
+		if (displayName2!=null) {
+			this.displayName = displayName2;
+		}
+		if (teeShirtSize2!=null) {
+			this.teeShirtSize = teeShirtSize2;
+		}
+	
+	}
 
 }
--- 03_lesson/ConferenceCentral/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
+++ 03_lesson/ConferenceCentral/src/main/java/com/google/devrel/training/conference/spi/ConferenceApi.java
@@ -1,7 +1,6 @@
 package com.google.devrel.training.conference.spi;
 
-
-//import static com.google.devrel.training.conference.service.OfyService.ofy;
+import static com.google.devrel.training.conference.service.OfyService.ofy;
 
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
@@ -12,109 +11,83 @@ import com.google.devrel.training.conference.Constants;
 import com.google.devrel.training.conference.domain.Profile;
 import com.google.devrel.training.conference.form.ProfileForm;
 import com.google.devrel.training.conference.form.ProfileForm.TeeShirtSize;
-//import com.googlecode.objectify.Key;
+import com.google.devrel.training.conference.service.OfyService;
+
+import com.googlecode.objectify.Key;
 
 /** Defines conference APIs. */
-@Api(name = "conference", version = "v1", scopes = { Constants.EMAIL_SCOPE }, 
-	clientIds = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID }, 
-	description = "API for the Conference Central Backend application.")
+@Api(name = "conference", version = "v1", scopes = { Constants.EMAIL_SCOPE }, clientIds = { Constants.WEB_CLIENT_ID,
+		Constants.API_EXPLORER_CLIENT_ID }, description = "API for the Conference Central Backend application.")
 public class ConferenceApi {
-    /* Get the display name from the user's email. For example, if the email is lemoncake@example.com, then the display name becomes "lemoncake." */
-    private static String extractDefaultDisplayNameFromEmail(String email) {
-        return email == null ? null : email.substring(0, email.indexOf("@"));
-    }
+	/*
+	 * Get the display name from the user's email. For example, if the email is
+	 * lemoncake@example.com, then the display name becomes "lemoncake."
+	 */
+	private static String extractDefaultDisplayNameFromEmail(String email) {
+		return email == null ? null : email.substring(0, email.indexOf("@"));
+	}
 
-    /**
-     * Creates or updates a Profile object associated with the given user
-     * object.
-     *
-     * @param user
-     *            A User object injected by the cloud endpoints.
-     * @param profileForm
-     *            A ProfileForm object sent from the client form.
-     * @return Profile object just created.
-     * @throws UnauthorizedException
-     *             when the User object is null.
-     */
+	/**
+	 * Creates or updates a Profile object associated with the given user
+	 * object.
+	 *
+	 * @param user
+	 *            A User object injected by the cloud endpoints.
+	 * @param profileForm
+	 *            A ProfileForm object sent from the client form.
+	 * @return Profile object just created.
+	 * @throws UnauthorizedException
+	 *             when the User object is null.
+	 */
 
-    // Declare this method as a method available externally through Endpoints
-    @ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
-    // The request that invokes this method should provide data that
-    // conforms to the fields defined in ProfileForm
+	@ApiMethod(name = "saveProfile", path = "profile", httpMethod = HttpMethod.POST)
+	public Profile saveProfile(final User user, ProfileForm profileForm) throws UnauthorizedException {
+		if (user == null) {
+			throw new UnauthorizedException("Authorization Required");
+		} 
+		String userId = user.getUserId();
+		String mainEmail = user.getEmail();
+		/* Create a new Profile entity from the userId, displayName, mainEmail and teeShirtSize */
+		Profile profile = this.getProfile(user);
+		if (profile == null) {
+			String displayName = null;
+			TeeShirtSize teeShirtSize = null;
+			if (profileForm.getTeeShirtSize()!= null) {
+				teeShirtSize = profileForm.getTeeShirtSize();
+			} else {
+				teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
+			}
+			if (profileForm.getDisplayName() != null) {
+				displayName = profileForm.getDisplayName();
+			} else {
+				displayName = extractDefaultDisplayNameFromEmail(mainEmail);
+			}
+			profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
+		} else {
+			profile.update(profileForm.getDisplayName(), profileForm.getTeeShirtSize());
+		}
+		ofy().save().entity(profile).now();
+		return profile;
+	}
 
-    // TODO 1 Pass the ProfileForm parameter
-    // TODO 2 Pass the User parameter
-    public Profile saveProfile(final User user, ProfileForm profileForm) throws UnauthorizedException {
-
-        String userId = null;
-        String mainEmail = null;
-        String displayName = "Your name will go here";
-        TeeShirtSize teeShirtSize = TeeShirtSize.NOT_SPECIFIED;
-
-        // TODO 2
-        // If the user is not logged in, throw an UnauthorizedException
-
-        // TODO 1
-        // Set the teeShirtSize to the value sent by the ProfileForm, if sent
-        // otherwise leave it as the default value
-        
-        
-
-        // TODO 1
-        /* Set the displayName to the value sent by the ProfileForm, if sent otherwise set it to null */
-        displayName = profileForm.getDisplayName();
-        if (profileForm.getTeeShirtSize()!=null) {
-        	teeShirtSize = profileForm.getTeeShirtSize();
-        }
-        
-        // TODO 2
-        // Get the userId and mainEmail
-        if (user==null) {
-        	throw new UnauthorizedException("Authorization Required");
-        } else {
-        	mainEmail = user.getEmail();
-        	userId = user.getUserId();
-        	if (displayName == null) {
-        		displayName = extractDefaultDisplayNameFromEmail(mainEmail);
-        	}
-        	
-        }
-        // TODO 2
-        // If the displayName is null, set it to default value based on the user's email
-        // by calling extractDefaultDisplayNameFromEmail(...)
-        
-
-        /* Create a new Profile entity from the userId, displayName, mainEmail and teeShirtSize */
-        Profile profile = new Profile(userId, displayName, mainEmail, teeShirtSize);
-
-        // TODO 3 (In Lesson 3)
-        // Save the Profile entity in the datastore
-
-        // Return the profile
-        return profile;
-    }
-
-    /**
-     * Returns a Profile object associated with the given user object. The cloud
-     * endpoints system automatically inject the User object.
-     *
-     * @param user
-     *            A User object injected by the cloud endpoints.
-     * @return Profile object.
-     * @throws UnauthorizedException
-     *             when the User object is null.
-     */
-    @ApiMethod(name = "getProfile", path = "profile", httpMethod = HttpMethod.GET)
-    public Profile getProfile(final User user) throws UnauthorizedException {
-        if (user == null) {
-            throw new UnauthorizedException("Authorization required");
-        }
-
-        // TODO
-        // load the Profile Entity
-        //String userId = ""; // TODO
-        //Key key = null; // TODO
-        Profile profile = null; // TODO load the Profile entity
-        return profile;
-    }
+	/**
+	 * Returns a Profile object associated with the given user object. The cloud
+	 * endpoints system automatically inject the User object.
+	 *
+	 * @param user
+	 *            A User object injected by the cloud endpoints.
+	 * @return Profile object.
+	 * @throws UnauthorizedException
+	 *             when the User object is null.
+	 */
+	@ApiMethod(name = "getProfile", path = "profile", httpMethod = HttpMethod.GET)
+	public Profile getProfile(final User user) throws UnauthorizedException {
+		if (user == null) {
+			throw new UnauthorizedException("Authorization required");
+		}
+		String userId = user.getUserId();
+		Key<Profile> key = Key.create(Profile.class, userId);
+		Profile profile = (Profile)ofy().load().key(key).now();
+		return profile;
+	}
 }
